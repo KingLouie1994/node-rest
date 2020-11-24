9d3592a5e9424f1b1b5b4ebef30e60a4391770ce
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-11-24 18:06:17 +0100

    user status works

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,9 @@ const { body } = require("express-validator");
 
 const authController = require("../controllers/auth");
 const User = require("../models/user");
+
+const isAuth = require("../middleware/is-auth");
+
 const router = express.Router();
 
 router.put(
@@ -26,4 +29,14 @@ router.put(
 );
 
 router.put("/login", authController.login);
+
+router.get("/status", isAuth, authController.getUserStatus);
+
+router.patch(
+  "/status",
+  isAuth,
+  [body("status").trim().not().isEmpty()],
+  authController.updateUserStatus
+);
+
 module.exports = router;
